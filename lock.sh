028d471ef1d1cb2cd7fa0844bfb7dffb9ffecd20
TheDzejkob <axo.lotl.original@gmail.com>
2026-01-28 09:31:27 +0100

    a

---
+++ lock.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+swaylock \
+  --daemonize \
+  --clock \
+  --screenshots \
+  --effect-blur 8x6 \
+  --effect-vignette 0.4:0.4 \
+  --color 282a36 \
+  --inside-color 282a3699 \
+  --ring-color bd93f9 \
+  --key-hl-color 50fa7b \
+  --bs-hl-color ff5555 \
+  --text-color f8f8f2 \
+  --line-color 00000000 \
+  --separator-color 00000000 \
+  --indicator-radius 100 \
+  --indicator-thickness 10
+
